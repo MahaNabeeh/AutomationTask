2509cd9b7ef716e0d216db1dd5eb3f8144214a32
MahaNabeeh <mahanabeeh33@yahoo.com>
2023-12-23 14:38:03 +0200

    changed to Ahmed

--- task1/src/Main.java
+++ task1/src/Main.java
@@ -3,7 +3,7 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Maha!");
+        System.out.println("Hello Ahmed!");
 
 
     }
